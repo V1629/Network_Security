9b55f324634b6279bbdb9fd1bc28bd83cf4e0b7a
V1629 <vaibhavmishraad794@gmail.com>
2025-11-17 11:16:48 +0530

    added model_trainer.py, added initiate_model_trainer

---
+++ network_security/components/model_trainer.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+from network_security.exceptions.exception import NetworkSecurityException
+from network_security.logging.logger import logging
+
+from network_security.entity.artifact_entity import DataTransformationArtifact, ModelTrainerArtifact
+from network_security.entity.config_entity import ModelTrainerConfig
+
+from network_security.utils.ml_utils.model.estimator import NetworkModel
+from network_security.utils.main_utils.utils import save_object, save_numpy_array_data, load_object, load_numpy_array_data
+from network_security.utils.ml_utils.metric.classification_metric import classification_score
+
+class ModelTrainer:
+    def __init__(self, model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
+        try:
+            self.model_trainer_config = model_trainer_config
+            self.data_transformation_artifact = data_transformation_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+
+    def train_model(self,x_train,y_train)
+        
+
+    def initiate_model_trainer(self)->ModelTrainerArtifact:
+        try:
+            train_file_path = self.data_transformation_artifact.transformed_train_file_path
+            test_file_path = self.data_transformation_artifact.transformed_test_file_path
+
+
+            #loading training array and testing array
+            train_arr = load_numpy_array_data(train_file_path)
+            test_arr=load_numpy_array_data(test_file_path)
+
+            x_train, y_train,x_test,y_test = (
+                train_arr[:,:-1],
+                train_arr[:,-1],
+                test_arr[:,:-1],
+                test_arr[:,-1],
+            )
+
+            model = self.train_model(x_train,y_train)
+
+
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
--- network_security/constants/training_pipeline/__init__.py
+++ network_security/constants/training_pipeline/__init__.py
@@ -18,6 +18,9 @@ TEST_FILE_NAME: str = "test.csv"
 
 SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
 
+SAVED_MODEL_DIR = os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
+
 """
 Data Ingestion related constant start with DATA_INGESTON VAR NAME
 """
@@ -48,6 +51,15 @@ Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed_object"
+
+"""
+Model Trainer related constant start with MODE TRAINER VAR NAME
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE:float = 0.6
+MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD: float = 0.05
 
 #knn inputers to replace nan values
 DATA_TRANSFORMATION_IMPUTER_PARAMS: dict ={
