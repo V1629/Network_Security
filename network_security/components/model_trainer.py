a3dcf5f08cabdd1de3213e46573acdf0a384d817
Vaibhav Mishra <167624631+V1629@users.noreply.github.com>
2025-11-19 14:39:29 +0530

    Update model hyperparameters for tuning

--- network_security/components/model_trainer.py
+++ network_security/components/model_trainer.py
@@ -42,16 +42,16 @@ class ModelTrainer:
                 'criterion':['gini','entropy','log_loss'],
             },
             "Random Forest":{
-                'n_estimators':[8,16,32,64,128,256]
+                'n_estimators':[8,16,32,128,256]
             },
             "Gradient Boosting": {
                 'learning_rate':[.1,.01,.05,.001],
-                'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                'subsample':[0.6,0.7,0.75,0.85,0.9],
                 'n_estimators': [8,16,32,64,128,256]
             },
             "Logistic Regression": {},
             "AdaBoost":{
-                'learning_rate':[.1,.01,0.5,0.05],
+                'learning_rate':[.1,.01,0.05],
                 'n_estimators': [8,16,32,64,128,256]
             }
         }
